bd7036c56293968f4af2d8f5b56df5f812977c8e
user <user@users-MacBook-Pro-2.local>
2016-07-28 08:26:52 -0500

    A skills test I'm working on for an employment page.

---
+++ public/Unloader_Material_Handler_Skills_Test.php
@@ -0,0 +1,19 @@
+<?php
+	var_dump($_GET)
+	var_dump($_POST)
+?>
+<!Doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<title>Team Member Knowledge</title>
+		</head>
+		<body>
+		<h2>Team Member Knowledge</h2>
+<form method="POST" action="http://requestb.in/rxh6scrx">
+				<label for="To:">To:</label>
+				<input id="username" name="username" type="text" Placeholder="Enter Recipient">
+				<label for="CC:">CC:</label>
+				<input id="username" name="username" type="text" Placeholder="Enter CC Recipient">
+				<label for="BCC:">BCC:</label>
+				<input id="username" name="username" type="text" Placeholder="Enter BCC Recipient ">
