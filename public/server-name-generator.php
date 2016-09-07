9dbdd5413f08dfc290b5cc3701579e8db3c0b93e
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-07 16:56:06 -0500

    Update: can't remember if I pushed.

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -10,18 +10,30 @@
 // 			Another containing at least 10 nouns.
 //////////////////////////////////////////////////////////////////////////////////
 
-$adjectiveArray = array("grand", "delicate", "woven", "soft", "light", "cool", "heavy", "bold", "electric", "radiant", "dark");
+// $adjectiveArray = array("grand", "delicate", "woven", "soft", "light", "cool", "heavy", "bold", "electric", "radiant", "dark");
 
 // $nounArray = array("willow", "oak", "fox", "grizzly", "cell", "soma", "metal", "mind", "skin", "energy", "matter");
 
 //////////////////////////////////////////////////////////////////////////////////
 // 3.  Create a function that will return a random element from an array.
 //////////////////////////////////////////////////////////////////////////////////
+	// $nounArray = array("willow", "oak", "fox", "grizzly", "cell", "soma", "metal", "mind", "aura", "energy", "matter");
+
+	// 	$min = 0;
+	// 	$maxAd = count($adjectiveArray) -1;
+	// 	$randomAd = mt_rand($min,$maxAd);
+	// 	return $adjectiveArray[$randomAd] . PHP_EOL;
+
+//////////////////////////////////////////////////////////////////////////////////
+// 4.  Create a function that returns the string value of our new server name made by  combining a random adjective with a random noun.
+// 		Each time you refresh the page a different result should be displayed.
+// 		Make sure you have fun with it. Add CSS to make it fancy.
+//////////////////////////////////////////////////////////////////////////////////
 
 	function getServerName() {
 		$adjectiveArray = array("grand", "delicate", "woven", "soft", "light", "cool", "heavy", "bold", "electric", "radiant", "dark");
 
-		$nounArray = array("willow", "oak", "fox", "grizzly", "cell", "soma", "metal", "mind", "skin", "energy", "matter");
+		$nounArray = array("willow", "oak", "fox", "grizzly", "cell", "soma", "metal", "mind", "aura", "energy", "matter");
 
 		$min = 0;
 		$maxAd = count($adjectiveArray) -1;
@@ -35,7 +47,7 @@ $adjectiveArray = array("grand", "delicate", "woven", "soft", "light", "cool", "
 	// echo "Your server name is " . getServerName($adjectiveArray, $nounArray) .  "." . PHP_EOL;
 
 	} 
-	// echo getServerName();
+	$serverName = getServerName();
 
 ?>
 <!DOCTYPE html>
@@ -45,7 +57,13 @@ $adjectiveArray = array("grand", "delicate", "woven", "soft", "light", "cool", "
 		<link rel="stylesheet" href="">
 	</head>
 	<body>
-		<h3>Your server name is <?php echo getServerName();?></h3>
+		<h3>Your server name is <?= $serverName;?></h3>
+		
+
+
+
+
+
 	</body>
 	</html>
 
