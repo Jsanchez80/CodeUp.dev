129f409cbd02e3655587e6a7579ca203a4e74501
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-09 15:08:33 -0500

    Update: Debugging.

--- public/php/login2.php
+++ public/php/login2.php
@@ -1,11 +1,37 @@
 <?php
 
+// var_dump($_POST); //Use to debug the username and password entries:
+
+function pageController()
+{
 // 1.  Create a file named login.php in your codeup.dev site. Add a form that accepts a username and password and have the form submit to the same page (login.php).
+$redirect = "/php/authorized.php";
 
 $username = isset($_POST['username']) ? $_POST['username']: '';
 $password = isset($_POST['password']) ? $_POST['password']: '';
 
-var_dump($_POST);
+// Check to see if the form was submitted without var_dumping: will post in the console:
+// place the term "die();" after the header in so that the process stops the operation.
+
+if(!empty($_POST)) {
+	if($username == "nico" && $password =="password") {
+		header("Location: $redirect");
+		die(); 
+	} elseif($username == '' || $password == '') {
+			echo("<script>alert('Please enter a valid username or password');</script>");	
+	} else {
+		echo("<script>alert('Login Failed! Try Again.');</script>");
+	}
+}
+
+	return [
+	'username' => $username,
+	'password' => $password,
+	];
+
+}	pageController();
+
+
 
 ?>
 
