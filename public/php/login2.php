9d3095cf043234ef190400b9d16518ecab3b3656
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-09 11:32:21 -0500

    Initial Commit of php folder

---
+++ public/php/login2.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="/css/login2.css">
+	</head> 
+	<body>
+		<div align="center">
+			<img src="/Img/bg.png" class="bg">
+		</div>
+	</body>
+</html>
