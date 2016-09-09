cbc7a9d8dcbc12712d6d3b183121a05e6d40cd75
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-09 18:25:16 -0500

    Update: I have the $_SESSION written to the code: functioning and prints username to body of the page.

--- public/php/authorized.php
+++ public/php/authorized.php
@@ -1,8 +1,43 @@
 <?php
 
+session_start();
 // <!-- Create a file named authorized.php in your codeup.dev site. Add some HTML that simply says "Authorized" when it is viewed in the browser. -->
 
-?>
+function pageController()
+{
+// 1.  Create a file named login.php in your codeup.dev site. Add a form that accepts a username and password and have the form submit to the same page (login.php).
+
+// Check to see if the form was submitted without var_dumping: will post in the console:
+// place the term "die();" after the header in so that the process stops the operation.
+$redirect = "/php/login2.php";		
+// Add a check to the authorized.php page to see if the user is logged in by checking the userIsLoggedIn key. If they are, redirect to the authorized.php page instead of showing the login page.
+$username = $_SESSION['Logged_in'];
+$password = ($_SESSION['userIsLoggedIn']);
+
+if ($_SESSION['Logged_in'] && $_SESSION['userIsLoggedIn'] == true) {
+	 
+	} elseif 
+		($username == '' || $password == '') {
+			header("Location: $redirect");	
+			die();
+	} 
+
+	return [
+	'username' => $username,
+	'password' => $password,
+	];
+	
+	
+	
+} 
+	extract(pageController());
+
+?> 
+ 
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,8 +46,9 @@
 		<link rel="stylesheet" href="/css/authorized.css">
 	</head> 
 	<body>
-	<h3>Welcome Nico!</h3>
-		
+	<div>
+	<h3>Welcome <?= $username ?></h3>
+	</div>
 		<div id="container" class="logInBox">       
 		<form method="POST">
 			<label for="username">Username:</label>
