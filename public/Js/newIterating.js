b880e23ae62bffd97980300236394d7047ac556b
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2017-05-30 22:06:19 -0500

    Working on a test array...

--- public/Js/newIterating.js
+++ public/Js/newIterating.js
@@ -2,17 +2,91 @@
     "use strict";
 
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-    var names = ['John', 'Erica', 'Diamond', 'Lexus']
+      var myDiagnosis = [
+      "Schizophrenia",
+      "Schizoaffective Disorder",
+      "Bipolar I Disorder",
+      "Bipolar II Disorder",
+      "Cyclothymic Disorder",
+      "Substance/Medication-Induced Bipolar and Related Disorder",
+      "Other Specified Bipolar and Related Disorder",
+      "Unspecified Bipolar and Related Disorder",
+      "Disruptive Mood Dysregulation Disorder",
+      "Major Depressive Disorder, Single and Recurrent Episodes",
+      "Persistent Depressive Disorder AKA Dysthymia",
+      "Premenstrual Dysphoric Disorder",
+      "Substance/Medication-Induced Depressive Disorder",
+      "Depressive Disorder Due to Another Medical Condition",
+      "Other Specified Depressive Disorder",
+      "Unspecified Depressive Disorder",
+      "Generallized Axiety Disorder",
+      "Substance/Medication-Induced Anxiety Disorder",
+      "Anxiety Disorder due to another Medical Condition",
+      "Other Specified Anxiety Disorder",
+      "Obsessive-Compulsive Disorder",
+      "Body Dismorphic Disorder",
+      "Hoarding Disorder",
+      "Excoriation Disorder/Skin-Picking",
+      "Substance/Medication-Induced Obsessive-Compulsive and Related Disorder",
+      "Other Specified Obsessive-Compulsive and Related Disorder",
+      "Unspecified Obsessive-Compulsive and Related Disorder",
+      "Reactive Attachment Disorder",
+      "Disinhibited Social Engagement Disorder",
+      "Posttraumatic Stress disorder",
+      "PTSD",
+      "Acute Stress Disorder",
+      "Adjustment Disorders",
+      "Other Specified Trauma-and Stressor-Related Disorder",
+      "Unspecified Trauma-and Stressor-Related Disorder"
+    ];
+
+    for (var i = 0; i < myDiagnosis.length; i++) {
+    	console.log('My declared illness ' + 'is ' + myDiagnosis[i]);
+    }
+
+    myDiagnosis.forEach(function (element, index, array){
+    	console.log('My diagnosis at index ' + index + ' is ' + element);
+    });
 
     // TODO: Create a log statement that will log the number of elements in the names array.
-    console.log(names.length);
+    console.log(myDiagnosis.length);
 
     // TODO: Create log statements that will print each of the names array elements individually.
-    console.log('Hi! My name is ' + names[0]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[0]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[1]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[2]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[3]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[4]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[5]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[6]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[7]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[8]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[9]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[10]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[11]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[12]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[13]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[14]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[15]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[16]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[17]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[18]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[19]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[20]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[21]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[22]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[23]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[24]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[25]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[26]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[27]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[28]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[29]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[30]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[31]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[32]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[33]);
+    console.log('Hi! My Diagnosis is ' + myDiagnosis[34]);
 
-    console.log('Hi! My name is ' + names[1]);
-
-    console.log('Hi! My name is ' + names[2]);
-
-    console.log('Hi! My name is ' + names[3]);
+    
 })();
