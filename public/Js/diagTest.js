19b985435d1cf4c684939c5ae274e2be3091c2a8
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2017-06-21 14:13:46 -0500

    Update: Working on the styling and funcionality of the page.

--- public/Js/diagTest.js
+++ public/Js/diagTest.js
@@ -1,35 +1,440 @@
 var soughtDiagnosis = [
     {
         subcategories: "Subcategory",
-        diagnosis: "Schizoprenia",
-        symptoms: ["Lemon", "Bluetooth Connectivity", "GPS Navigation"],
+        diagnosis: "Schizoprenia:",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
         organization: {
-            name: "SA Clubhouse",
-            streetAddress: "1234 Citizen's Pkwy",
-            city: "San Antonio", 
-            state: "TX"
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
         }
     },
     {
         subcategories: "Subcategory",
-        diagnosis: "Schizoaffective",
-        symptoms: ["Lemon", "Bluetooth Connectivity", "GPS Navigation"],
+        diagnosis: "Post Traumatic Stress Disorder (PTSD):",
+        symptoms: ["Periods of manic mood or a sudden increase in energy", " Symptoms of depression", " Hallucinations", " Impaired communication", " Impaired occupational, academic and social functioning"],
         organization: {
-            name: "Tyrese",
-            streetAddress: "1234 Citizen's Pkwy",
-            city: "San Antonio", 
-            state: "TX"
+            name: "Tyrese ",
+            streetAddress: "111 San Fernando, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "77009"
         }
-    }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
+    {
+        subcategories: "Subcategory",
+        diagnosis: "Schizoprenia",
+        symptoms: ["Withdrawal", " Lack of Emotions", " Insomnia", " Unexplained Hostility", " Sudden Lack of Hygiene"],
+        organization: {
+            name: "SA Clubhouse ",
+            streetAddress: "1234 Citizen's Pkwy, ",
+            city: "San Antonio, ", 
+            state: "TX",
+            zip: "78229"
+        }
+    },
 ];
 
-console.log("The first diagnosis is " + soughtDiagnosis[0].diagnosis + " " + soughtDiagnosis[0].symptoms + ".");
-console.log("Discover the following resources to learn more " + soughtDiagnosis[1].organization.name + soughtDiagnosis[1].organization.streetAddress + soughtDiagnosis[1].organization.city + soughtDiagnosis[1].organization.state);
-// console.log("The owner of the second car is " + cars[1].owner.name + ".");
 
-console.log("Here are all the symptoms of all the diagnosis:");
-soughtDiagnosis.forEach(function(soughtDiagnosis) {
-    soughtDiagnosis.symptoms.forEach(function(symptoms) {
-        console.log(symptoms);
-    });
-});
+
+
+console.log("Your diagnosis is " + soughtDiagnosis[0].diagnosis + " Some of the symptoms include  " + soughtDiagnosis[0].symptoms + ".");
+console.log("Discover the following resources to learn more " + soughtDiagnosis[0].organization.name + soughtDiagnosis[0].organization.streetAddress + soughtDiagnosis[0].organization.city + soughtDiagnosis[0].organization.state + soughtDiagnosis[0].organization.zip);
+// console.log("Your diagnosis is " + soughtDiagnosis[1].diagnosis + " Some of the symptoms include  " + soughtDiagnosis[0].symptoms + ".");
+// console.log("Discover the following resources to learn more " + soughtDiagnosis[1].organization.name + soughtDiagnosis[1].organization.streetAddress + soughtDiagnosis[1].organization.city + soughtDiagnosis[1].organization.state + soughtDiagnosis[1].organization.zip);
+
+
+// For Each Loop runs through the list of symptoms and prints a list of symptoms for each iteration within the array. 
+// console.log("Here are all the symptoms of all the diagnosis:");
+// soughtDiagnosis.forEach(function(soughtDiagnosis) {
+//     soughtDiagnosis.symptoms.forEach(function(symptoms) {
+//         console.log(symptoms);
+//  
+   // });
+// });
